920d40557a1e0987fae0677f1ee4aa4b6277080d
A LEXXX <a.s.solomatov@gmail.com>
2026-09-03 14:50:34 +0200

    Policies Upload

--- shared/js/includes.js
+++ shared/js/includes.js
@@ -2,10 +2,22 @@ async function loadBlock(id, file) {
     const el = document.getElementById(id);
     if (!el) return;
 
-    const res = await fetch(file);
-    const html = await res.text();
-    el.innerHTML = html;
+    try {
+        const res = await fetch(file);
+
+        if (!res.ok) {
+            throw new Error(
+                `Failed to load ${file}: ${res.status}`
+            );
+        }
+
+        const html = await res.text();
+        el.innerHTML = html;
+    } catch (error) {
+        console.error(error);
+    }
 }
 
-loadBlock("header", "../shared/html/header.html");
-loadBlock("footer", "../shared/html/footer.html");
+loadBlock("header", "/shared/html/header.html");
+loadBlock("legal-header", "/shared/html/legal-header.html");
+loadBlock("footer", "/shared/html/footer.html");
